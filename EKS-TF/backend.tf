3f4ca1a4c0a1ed64584879c92ea122e0f961da74
Shubham Gupta <53033920+mr-blogger-sg@users.noreply.github.com>
2024-06-30 15:32:38 -0400

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
+    bucket = "mariobucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-1" #Check your region 
   }
 }
